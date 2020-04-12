728ccb4669f9c275ffb34d84bfc6c0c85164b7f5
oanser <anseromar@outlook.fr>
2020-04-12 19:42:12 +0200

    first test passed

--- codingame/car_challenge.sh
+++ codingame/car_challenge.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sol=true
 filename="simple_car_challenge.txt"
 
 is_car()
 {
-    if [[ $1 =~ "|" ]];then echo -1; else echo $1; fi
+    if [[ $1 =~ ^"|"$ ]];then echo -1; else echo $1; fi
 }
 
 {
@@ -15,13 +14,19 @@ is_car()
 
     if [[ $bottom_left == $bottom_right ]];then echo "false"; exit; fi
     while read -r line_1; read line_2 ;do
-        if [[ is_car ${line_1:0:1} == -1 ]];then
-            if [[ is_car $(line_1:1:1} == -1 ]];then
+        if [[ $(is_car ${line_1:0:1}) -eq -1 ]];then
+            if [[ $(is_car ${line_1:1:1}) -eq -1 ]];then
                 continue
-            elif [[ line_1:1:1 > $bottom_left ]] && [[ -n line_2 && line_1:1:1 < is_car ${line_2:0:1} ]];then
+            elif [[ line_1:1:1 > $bottom_left || (-n $line_2 && ${line_1:1:1} < $(is_car ${line_2:0:1})) ]];then
                 echo "false"
+                exit
             fi
-       fi
+       elif [[ ${line_1:0:1} == $(is_car ${line_1:1:1}) || ${line_1:0:1} > $bottom_right || ( -n $line_2 && ${line_1:0:1} < $(is_car ${line_2:1:1})) ]];then
+            echo "false"
+            exit
+        fi
+    bottom_left=${line_1:0:1}
+    bottom_right=${line_1:1:1}
     done
 }< $filename
 
