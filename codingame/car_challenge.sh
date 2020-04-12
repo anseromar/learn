277ab4e3e0bc1f91f908e6e4d1bb7b3755d821dd
oanser <anseromar@outlook.fr>
2020-04-12 21:34:52 +0200

    tchaz

--- codingame/car_challenge.sh
+++ codingame/car_challenge.sh
@@ -4,30 +4,36 @@ filename="simple_car_challenge.txt"
 
 is_car()
 {
-    if [[ $1 =~ ^"|"$ ]];then echo -1; else echo $1; fi
+    if [[ $1 =~ ^"|"$ ]];then echo -1; else echo "$1"; fi
 }
 
 {
     read -r firstline
-    bottom_left=$(is_car ${firstline:0:1})
-    bottom_right=$(is_car ${firstline:1:1})
+    top_left=$(is_car "${firstline:0:1}")
+    top_right=$(is_car "${firstline:1:1}")
 
-    if [[ $bottom_left == $bottom_right ]];then echo "false"; exit; fi
-    while read -r line_1; read line_2 ;do
-        if [[ $(is_car ${line_1:0:1}) -eq -1 ]];then
-            if [[ $(is_car ${line_1:1:1}) -eq -1 ]];then
+    if [[ "$top_left" == "$top_right" ]];then echo "false"; exit; fi
+    while read -r line_1; read -r line_2;do
+       if [[ $(is_car "${line_1:0:1}") -eq -1 ]];then
+            if [[ $(is_car "${line_1:1:1}") -eq -1 ]];then
                 continue
-            elif [[ line_1:1:1 > $bottom_left || (-n $line_2 && ${line_1:1:1} < $(is_car ${line_2:0:1})) ]];then
+            elif [[ line_1:1:1 > $top_left || (-n $line_2 && "${line_1:1:1}" < $(is_car "${line_2:0:1}")) ]];then
                 echo "false"
-                exit
             fi
-       elif [[ ${line_1:0:1} == $(is_car ${line_1:1:1}) || ${line_1:0:1} > $bottom_right || ( -n $line_2 && ${line_1:0:1} < $(is_car ${line_2:1:1})) ]];then
+        elif [[ "${line_1:0:1}" == $(is_car "${line_1:1:1}") || ${line_1:0:1} > $top_right || ( -n $line_2 && "${line_1:0:1}" < $(is_car "${line_2:1:1}")) ]];then
             echo "false"
-            exit
-        fi
-    bottom_left=${line_1:0:1}
-    bottom_right=${line_1:1:1}
+           fi
+    top_left="${line_1:0:1}"
+    top_right="${line_1:1:1}"
     done
+
+    read -r lastline
+    echo "$lastline"
+    bottom_left=$(is_car "${lastline:0:1}")
+    bottom_right=$(is_car "${lastline:1:1}")
+    echo "$bottom_left"
+    echo "$bottom_right"
+
 }< $filename
 
 
